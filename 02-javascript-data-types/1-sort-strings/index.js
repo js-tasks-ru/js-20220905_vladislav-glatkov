b82c8f5f904c56dc2e4e9d8ae05ecf834e44f2b7
Vladislav Glatkov <trufors@gmail.com>
2022-09-12 20:35:38 +0300

    fixed ;

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -6,16 +6,16 @@
  */
 export function sortStrings(arr, param = 'asc') {
   if (param !== 'asc' && param !== 'desc') {
-    console.log('sort.error')
+    console.log('sort.error');
     return; 
   }
-  
+
   let direction = 1;
 
   if (param === 'desc') {
     direction = -1;
   }
   return [...arr].sort((a, b)=> {
-    return direction * a.localeCompare(b, ['ru', 'en'], {caseFirst: upper})
+    return direction * a.localeCompare(b, ['ru', 'en'], {caseFirst: 'upper'});
   });
 }
