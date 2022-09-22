18b281ba8dfcb7a48eaf3d3271aff8159ea0deac
Vladislav Glatkov <trufors@gmail.com>
2022-09-22 16:38:40 +0300

    5-tasks

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,54 @@
 export default class NotificationMessage {
+  constructor(message = '', {type = 'error', duration = 0} = {}) {
+    this.message = message;
+    this.type = type;
+    this.duration = duration;
+    this.render();
+  }
+ 
+  clearTimer() {
+    if (NotificationMessage.timer) {
+      clearTimeout(NotificationMessage.timer);
+    }
+  }
+  checkElementNotification() {
+    if (NotificationMessage.element) {
+      NotificationMessage.element?.remove();
+    }
+    this.clearTimer();
+  }
+  getTemplate() { 
+    const wrapper = document.createElement('div');
+    wrapper.innerHTML = `
+    <div class="notification ${this.type}" style="--value:${this.duration / 1000}s">
+    <div class="timer"></div>
+    <div class="inner-wrapper">
+      <div class="notification-header">${this.type}</div>
+        <div class="notification-body">
+          ${this.message}
+        </div>
+      </div>
+      </div>
+     `;
+    return wrapper.firstElementChild;
 
+  }
+  render() {
+    this.checkElementNotification();
+    this.element = this.getTemplate();
+    NotificationMessage.element = this.element;
+    
+  }
+  show(element = document.body) { 
+    element.append(NotificationMessage.element);
+    NotificationMessage.timer = setTimeout(() => this.remove(), this.duration);
+  }
+  remove() {
+    this.element?.remove();
+    this.clearTimer();
+
+  }
+  destroy() {
+    this.remove();
+  }
 }
